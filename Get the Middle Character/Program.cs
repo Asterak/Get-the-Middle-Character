06ccd482589d5801dbacdfb6e88b63bedf2184ae
Сергей Гупалов <asterak@gmail.com>
2020-02-23 11:14:22 +0300

    Add project files.

---
+++ Get the Middle Character/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Get_the_Middle_Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
